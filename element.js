47ab257a594eda0a3cde721f56c65b06778fba47
themmixproject <steven.devalk@gmail.com>
2020-05-17 18:43:11 +0200

    added element class function

--- element.js
+++ element.js
@@ -1,1 +1,13 @@
 console.log("element.js file is loaded!");
+function element (elementTag, elementId, elementClass) {
+    this.tag = elementTag;
+    if(this.tag==""){
+        this.tag="div";
+    }
+    this.id = elementId;
+    this.class = elementClass;
+}
+
+var myElement = new element("", "elementStuff", "myClass");
+
+console.log(myElement);
