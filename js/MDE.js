ed48aa3a0d8542e2e43f4dcf97c7448b6597922a
themmixproject <steven.devalk@gmail.com>
2020-07-08 00:45:29 +0200

    createElementForm() method added  - global editorPageContent varialbe added  - debug run of createElementForm() added

--- js/MDE.js
+++ js/MDE.js
@@ -33,6 +33,7 @@ var creationAddButton = document.getElementById("element-creation-add-button");
 
 var creationCancelButton = document.getElementById("element-creation-cancel-button");
 
+var editorPageContent = document.getElementById("editor-page-content");
 
 /*#####################################################\
  *|                                                    #
@@ -114,7 +115,103 @@ function createElementListItem(tag, id, className){
 
 elementListContainer.append(createElementListItem("div", "testId", "testClass"))
 
+function createElementForm(){
 
+    var form = document.createElement("div");
+    form.className = "generic-container";
+    form.setAttribute("id", "element-creation-form");
+
+
+
+    // create id input
+    var inputContainer = document.createElement("div");
+    inputContainer.className = "element-creation-input-container";
+    
+    var inputLabel = document.createElement("div");
+    inputLabel.className = "element-creation-input-label";
+    
+    var inputLabelValue = document.createElement("span");
+    inputLabelValue.className = "code-constant-attribute";
+    inputLabelValue.innerHTML+="id";
+    inputLabel.append(inputLabelValue);
+
+    inputLabel.innerHTML+=" =&nbsp;"
+    
+    inputContainer.append(inputLabel);
+
+    var inputWrapper = document.createElement("span");
+    inputWrapper.className = "element-creation-input-width-wrapper";
+    inputContainer.append(inputWrapper);
+
+    var input = document.createElement("input");
+    input.className = "element-creation-input code-string";
+
+    inputWrapper.append(input);
+
+    form.append(inputContainer);
+
+
+
+    // create class input
+    var inputContainer = document.createElement("div");
+    inputContainer.className = "element-creation-input-container";
+    
+    var inputLabel = document.createElement("div");
+    inputLabel.className = "element-creation-input-label";
+    
+    var inputLabelValue = document.createElement("span");
+    inputLabelValue.className = "code-constant-attribute";
+    inputLabelValue.innerHTML+="class";
+    inputLabel.append(inputLabelValue);
+
+    inputLabel.innerHTML+=" =&nbsp;"
+    
+    inputContainer.append(inputLabel);
+
+    var inputWrapper = document.createElement("span");
+    inputWrapper.className = "element-creation-input-width-wrapper";
+    inputContainer.append(inputWrapper);
+
+    var input = document.createElement("input");
+    input.className = "element-creation-input code-string";
+
+    inputWrapper.append(input);
+
+    form.append(inputContainer);
+
+    // create half button UI
+    var buttonContainer = document.createElement("div");
+    buttonContainer.className = "half-width-container";
+
+    // create cancel button
+    var buttonWrapper = document.createElement("div");
+    buttonWrapper.className = "half-width-wrapper";
+    buttonContainer.append(buttonWrapper);
+
+    var button = document.createElement("div");
+    button.className = "generic-ui-button half-width-margin-right";
+    buttonWrapper.append(button);
+    button.setAttribute("id", "element-creation-cancel-button");
+    button.innerHTML+="cancel";
+
+    // create cancel button
+    var buttonWrapper = document.createElement("div");
+    buttonWrapper.className = "half-width-wrapper";
+    buttonContainer.append(buttonWrapper);
+
+    var button = document.createElement("div");
+    button.className = "generic-ui-button half-width-margin-right";
+    buttonWrapper.append(button);
+    button.setAttribute("id", "element-creation-add-button");
+    button.innerHTML+="add";
+
+    form.append(buttonContainer);
+
+    return form;
+
+}
+
+editorPageContent.append(createElementForm());
 
 /*#####################################################\
  *|                                                    #
